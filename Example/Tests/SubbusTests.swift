1db4dfbe9972b3b8e648f447b71f533feadac4e3
Alex Larson <alarson@myriadmobile.com>
2020-02-03 16:36:41 -0600

    Removed optional support. I'll just display a console output.

--- Example/Tests/SubbusTests.swift
+++ Example/Tests/SubbusTests.swift
@@ -15,186 +15,102 @@ class TestClassC{}
 
 class SubbusTests: XCTestCase {
     
-//    //Simple flow.
-//    func test1() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: self, event: TestClassA.self)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 1)
-//    }
-//
-//    //Simple flow. Broad unsubscribe.
-//    func test2() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: self)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 1)
-//    }
-//
-//    //Dual subscription.
-//    func test7() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: self)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 2)
-//    }
-//
-//    //Dual subscription. Different events.
-//    func test10() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.subscribe(id: self, event: TestClassB.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: self)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 1)
-//    }
-//
-//    //Dual subscription. Single unsubscribe.
-//    func test12() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.subscribe(id: self, event: TestClassB.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: self, event: TestClassA.self)
-//        Subbus.post(event: TestClassB())
-//
-//        //End condition
-//        XCTAssertEqual(count, 2)
-//    }
-//
-//    //Simple flow. Dual fire.
-//    func test13() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: self, event: TestClassA.self)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 2)
-//    }
-//
-//    //Dual subscription with same IDs.
-//    func test14() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: 1, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.subscribe(id: 1, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: 1)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 2)
-//    }
-//
-//    //Dual subscription with different IDs. Unsub both.
-//    func test15() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: 1, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.subscribe(id: 2, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: 1)
-//        Subbus.unsubscribe(id: 2)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 2)
-//    }
-//
-//    //Dual subscription with different IDs. Unsub one.
-//    func test16() {
-//        var count = 0
-//
-//        //Test
-//        Subbus.subscribe(id: 1, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.subscribe(id: 2, event: TestClassA.self) { (event) in
-//            count += 1
-//        }
-//
-//        Subbus.post(event: TestClassA())
-//        Subbus.unsubscribe(id: 1)
-//        Subbus.post(event: TestClassA())
-//
-//        //End condition
-//        XCTAssertEqual(count, 3)
-//    }
+    //Test Basic Functionality
+    func testEventUnsubscribe() {
+        var count = 0
+
+        //Test
+        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
+            count += 1
+        }
+
+        Subbus.post(event: TestClassA())
+        Subbus.unsubscribe(id: self, event: TestClassA.self)
+        Subbus.post(event: TestClassA())
+
+        //End condition
+        XCTAssertEqual(count, 1)
+    }
+
+    func testDualSubscription() {
+        var count = 0
+
+        //Test
+        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
+            count += 1
+        }
+
+        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
+            count += 1
+        }
+
+        Subbus.post(event: TestClassA())
+        Subbus.unsubscribe(id: self)
+        Subbus.post(event: TestClassA())
+
+        //End condition
+        XCTAssertEqual(count, 2)
+    }
+
+    func testMutlipleEventsSameID() {
+        var count = 0
+
+        //Test
+        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
+            count += 1
+        }
+
+        Subbus.subscribe(id: self, event: TestClassB.self) { (event) in
+            count += 1
+        }
+
+        Subbus.post(event: TestClassA())
+        Subbus.unsubscribe(id: self)
+        Subbus.post(event: TestClassA())
+
+        //End condition
+        XCTAssertEqual(count, 1)
+    }
+
+    func testMultipleEventsSingleUnsubscribe() {
+        var count = 0
+
+        //Test
+        Subbus.subscribe(id: self, event: TestClassA.self) { (event) in
+            count += 1
+        }
+
+        Subbus.subscribe(id: self, event: TestClassB.self) { (event) in
+            count += 1
+        }
+
+        Subbus.post(event: TestClassA())
+        Subbus.unsubscribe(id: self, event: TestClassA.self)
+        Subbus.post(event: TestClassB())
+
+        //End condition
+        XCTAssertEqual(count, 2)
+    }
+
+    func testTestMultiplePost() {
+        var count = 0
+
+        //Test
+        let myID: Int? = 1
+        Subbus.subscribe(id: myID, event: TestClassA.self) { (event) in
+            count += 1
+        }
+
+        Subbus.post(event: TestClassA())
+        Subbus.post(event: TestClassA())
+        Subbus.unsubscribe(id: self, event: TestClassA.self)
+        Subbus.post(event: TestClassA())
+
+        //End condition
+        XCTAssertEqual(count, 2)
+    }
     
-    //Non-optional id types
+    //Test ID Types
     func testStringID() {
         var count = 0
         
@@ -272,166 +188,6 @@ class SubbusTests: XCTestCase {
         Subbus.post(event: TestClassA())
         
         XCTAssertEqual(count, 1)
-    }
-    
-    //Optional ID types
-    func testOptionalStringID() {
-        var count = 0
-        
-        let id: String? = "1"
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 1)
-    }
-    
-    func testOptionalIntID() {
-        var count = 0
-        
-        let id: Int? = 1
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 1)
-    }
-    
-    func testOptionalNSObjectID() {
-        var count = 0
-        
-        let id: NSObject? = NSObject()
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 1)
-    }
-    
-    func testOptionalStructID() {
-        struct FoobarStruct { }
-        
-        var count = 0
-        
-        let id: FoobarStruct? = FoobarStruct()
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 1)
-    }
-    
-    func testOptionalClassID() {
-        class FoobarClass { }
-        
-        var count = 0
-        
-        let id: FoobarClass? = FoobarClass()
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 1)
-    }
-    
-    //Nil IDs
-    func testNilStringID() {
-        var count = 0
-        
-        let id: String? = nil
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 0)
-    }
-    
-    func testNilIntID() {
-        var count = 0
-        
-        let id: Int? = nil
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 0)
-    }
-    
-    func testNilNSObjectID() {
-        var count = 0
-        
-        let id: NSObject? = nil
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 0)
-    }
-    
-    func testNilStructID() {
-        struct FoobarStruct { }
-        
-        var count = 0
-        
-        let id: FoobarStruct? = nil
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 0)
-    }
-    
-    func testNilClassID() {
-        class FoobarClass { }
-        
-        var count = 0
-        
-        let id: FoobarClass? = nil
-        Subbus.subscribe(id: id, event: TestClassA.self) { (event) in
-            count += 1
-        }
-
-        Subbus.post(event: TestClassA())
-        Subbus.unsubscribe(id: id)
-        Subbus.post(event: TestClassA())
-        
-        XCTAssertEqual(count, 0)
     }
     
 }
--- Subbus/Classes/Subbus.swift
+++ Subbus/Classes/Subbus.swift
@@ -24,8 +24,8 @@ fileprivate class ListenerWrapper {
 //Implementation
 public class Subbus {
     
+    //State
     private static let shared = Subbus()
-    
     private var listeners = [ListenerWrapper]() //TODO: Dictionary key of EventName with array of listeners - would be more performant
     private let eventbusCenter = NotificationCenter()
     
@@ -40,6 +40,9 @@ public class Subbus {
     //Subscribe
     //TODO: Keep an eye out for a compile-time error to enforce non-optional ids. Optional<Wrapped> is a type and thus the language doesn't seem to support that :(
     public static func subscribe<I, T>(id: I, event: T.Type, callback: @escaping (T) -> Void) {
+        //Verify data
+        guard (id as? OptionalProtocol) == nil else { log("Subscribe - You may not pass in an optional ID."); return }
+        
         //Define strings
         guard let identifier = stringFor(id: id) else { log("Subscribe - String representation of ID is empty."); return }
         let eventName = String(reflecting: event)
@@ -61,6 +64,9 @@ public class Subbus {
     
     //Unsubscribe
     public static func unsubscribe<I, T>(id: I, event: T.Type) {
+        //Verify data
+        guard (id as? OptionalProtocol) == nil else { log("Subscribe - You may not pass in an optional ID."); return }
+        
         //Define strings
         guard let identifier = stringFor(id: id) else { log("Unsubscribe - String representation of ID is empty."); return }
         let eventName = String(reflecting: event)
@@ -83,6 +89,9 @@ public class Subbus {
     }
     
     public static func unsubscribe<I>(id: I) {
+        //Verify data
+        guard (id as? OptionalProtocol) == nil else { log("Subscribe - You may not pass in an optional ID."); return }
+        
         //Define strings
         guard let identifier = stringFor(id: id) else { log("Unsubscribe - String representation of ID is empty."); return }
         
@@ -113,17 +122,10 @@ public class Subbus {
     }
     
     private static func stringFor<I>(id: I) -> String? {
-        //Generics seem to allow optional (because Optional<W> is a type)
-        //This we need to check if the type is optional and, if so, verify that the data is not nil.
-        //If it is nil - then return immediately.
-        if let idO = id as? OptionalProtocol, idO.hasValue() == false { return nil }
-        
-        //Components for return string
-        let unwrappedType = self.unwrappedType(of: id)
         var valString = "" //Value or pointer
         
         //Get the right string type; objects need memory address and primitives just need value.
-        if unwrappedType is AnyClass {
+        if I.self is AnyClass {
             valString = "\(Unmanaged.passUnretained(id as AnyObject).toOpaque())"
         } else {
             valString = "\(id)"
@@ -133,34 +135,13 @@ public class Subbus {
         guard valString.isEmpty == false else { return nil }
         guard valString != "nil" else { return nil }
         
-        let name = "\(unwrappedType)-\(valString)"
+        let name = "\(I.self)-\(valString)"
         print("NAME: \(name)")
         return name
     }
     
-    static func unwrappedType<I>(of object: I) -> Any.Type {
-        if let objectOptional = object as? OptionalProtocol {
-            return objectOptional.wrappedType()
-        } else {
-            return I.self
-        }
-    }
-    
 }
 
-protocol OptionalProtocol {
-    func wrappedType() -> Any.Type
-    func hasValue() -> Bool
-}
-
-extension Optional: OptionalProtocol {
-    func wrappedType() -> Any.Type {
-        return Wrapped.self
-    }
-    
-    func hasValue() -> Bool {
-        var hasValue = false
-        self.map({ $0; hasValue = true })
-        return hasValue
-    }
-}
+//An easy way to check if something is optional without knowing the Wrapped type
+protocol OptionalProtocol {}
+extension Optional: OptionalProtocol {}
